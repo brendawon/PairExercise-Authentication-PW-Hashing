b484fd0d432e55ed65561e14f0200242ea9f74a2
Brenda Wong <Brendot.wong@gmail.com>
2021-05-24 12:48:03 -0400

    JWT set up

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const {
+  models: { User },
+} = require("./db");
+const path = require("path");
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+
+// api auth sent w/ password; server generates JWT
+app.post("/api/auth", async (req, res, next) => {
+  try {
+    res.send({ token: await User.authenticate(req.body) });
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+// sends JWT and server verifies and reponds w/ data
+app.get("/api/auth", async (req, res, next) => {
+  try {
+    res.send(await User.byToken(req.headers.authorization));
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send({ error: err.message });
+});
+
+module.exports = app;
